6fd3b4760416bbe082b47e8a7e558a4d04606703
Allowexe <106865010+Allowexe@users.noreply.github.com>
2025-05-13 15:33:58 +0200

    Optimisation pour mon PC, comme ça la démo est cool

--- src/start_safebox.erl
+++ src/start_safebox.erl
@@ -5,7 +5,7 @@ start() ->
     code:add_patha("ebin"),
     application:start(crypto),
     safebox_node:start(),
-    safebox_net:set_nodes(['node1@localhost', 'node2@localhost', 'node3@localhost']),
+    safebox_net:set_nodes(['node1@HighPowerDingus', 'node2@HighPowerDingus', 'node3@HighPowerDingus']),
     safebox_cli:start(),
     %% Empêche de revenir à l'interpréteur après le CLI
     timer:sleep(infinity).
